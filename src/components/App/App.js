fdc2fb080fe3618c32784092e86b6c6632da3288
JuanOcampo <75342975+juanocampo0715@users.noreply.github.com>
2021-10-09 17:46:11 -0500

    Add files via upload

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from '../Header/Header';
+import MenuOption from '../MenuOption/MenuOption';
+import VentasOk from '../VentasOk/VentasOk';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MenuOption />
+      <VentasOk />
+      
+    </>
+  );
+}
+
+export default App;
